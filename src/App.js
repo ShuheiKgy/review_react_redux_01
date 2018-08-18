76cb22ba40631608b9c7b7c147883f8a9aebff91
Shuhei Kagaya <shuhei.lab@gmail.com>
2018-08-18 22:32:54 +0900

    App.jsの修正

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Redux ToDo List</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {this.props.todos.map(todo => {
+            return (
+              <li key={todo}>{todo}</li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
